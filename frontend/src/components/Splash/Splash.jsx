a801c90c18e6133c53167bd0ec452614e4e29dd8
udita <uditagowdety@gmail.com>
2025-01-07 11:31:25 +0530

    remove root css, add sections to splash

---
+++ frontend/src/components/Splash/Splash.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import splashImage from "../assets/splash.jpeg";
+import "./splash.css"
+
+const SplashPage = () => {
+  return (
+    <div className="container">
+        <div className="left">left section</div>
+        <div className="right">right section</div>
+    </div>
+  );
+};
+
+export default SplashPage;
