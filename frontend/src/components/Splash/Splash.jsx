b4b6f7c390900432b9d9203582d3f6670e1a54f7
udita <uditagowdety@gmail.com>
2025-01-10 17:15:24 +0530

    add buttons, style splash page text

--- frontend/src/components/Splash/Splash.jsx
+++ frontend/src/components/Splash/Splash.jsx
@@ -5,8 +5,22 @@ import "./splash.css"
 const SplashPage = () => {
   return (
     <div className="container">
-        <div className="left">left section</div>
-        <div className="right">right section</div>
+        <div className="left">
+          
+          <div className="text__container">
+            <h2 className="text__box project__title">CodeMaze</h2>
+            <h1 className="text__box project__subtitle">Navigating Code, One Problem at a Time</h1>
+            <div className="text__box project__tagline">Short Intro Text</div>
+          </div>
+          
+          <div className="button__container">
+            <button>login</button>
+            <button>guest</button>
+          </div>
+
+          
+        </div>
+        <div className="right"></div>
     </div>
   );
 };
