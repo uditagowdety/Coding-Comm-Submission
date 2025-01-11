98865daee5000d01767d62348164cdfb9a806264
udita <uditagowdety@gmail.com>
2025-01-11 14:39:22 +0530

    initialise login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Splash from './components/Splash/Splash'
+import Login from './components/Login/Login'
 
 function App() {
   
   return (
     <>
-      <Splash />
+      <Login />
     </>
   )
 }
