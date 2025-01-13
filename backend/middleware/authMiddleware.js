b7f9a4b66bdd8409a7f809db5d05417a6def1bc3
Adel <adeldsouza8@gmail.com>
2025-01-14 00:55:50 +0530

    integrating frontend into backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new UnauthenticatedError('No token provided')
+    throw new CustomError('No token provided')
   }
 
   const token = authHeader.split(' ')[1] // Extract token after "Bearer"
